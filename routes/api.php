4eb12d11d9adbafed3935ed7d8e7de7df3a1957f
YoussefErremili <yousseferremili.dev@gmail.com>
2026-04-04 01:42:57 +0100

    feat: finished auth system -- logout feature

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('register', RegisterController::class);
         Route::post('login', LoginController::class);
+        Route::post('logout', LogoutController::class)->middleware('auth:sanctum');
     });
 
 });
