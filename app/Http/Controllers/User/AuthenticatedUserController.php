a4108e3f2701b96dfbf2491c4d3ba9dbfc2dd4f1
YoussefErremili <yousseferremili.dev@gmail.com>
2026-04-17 01:32:00 +0100

    refactor: change path of Auth class to User Directory

--- app/Http/Controllers/User/AuthenticatedUserController.php
+++ app/Http/Controllers/User/AuthenticatedUserController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\User;
 
+use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use App\Support\ApiMessages;
 use App\Support\ApiResponse;
